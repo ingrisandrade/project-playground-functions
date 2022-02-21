4d2397c12c95c2985569a381681299b1c06fce9b
Ingris Andrade <iac22@outlook.com>
2022-02-21 16:55:56 -0300

    Função com número de telefone, criada.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,37 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeroTelefone) {
+  let telefone = '(' + numeroTelefone[0] + numeroTelefone[1] + ')' +
+  ' ' + numeroTelefone[2] + numeroTelefone[3] + numeroTelefone[4] + numeroTelefone[5] + numeroTelefone[6] + '-' +
+  numeroTelefone[7] + numeroTelefone[8] + numeroTelefone[9] + numeroTelefone[10];
+
+  console.log(typeof telefone);
+  let contador = 0;
+  if (numeroTelefone.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < numeroTelefone.length; index += 1) {
+    let arrayTotal = numeroTelefone[index];
+    if (numeroTelefone[index] < 0 || numeroTelefone[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  for (let indice = 0; indice < numeroTelefone.length; indice += 1) {
+    let arrayTotal = numeroTelefone[indice];
+    if (numeroTelefone[indice] === numeroTelefone[index]) {
+      contador += 1;
+    } if (contador >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  contador = 0;
+  }
+  return telefone;
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 0]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 1, 1, 0, 1]));
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
